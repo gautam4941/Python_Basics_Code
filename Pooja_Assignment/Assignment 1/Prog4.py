9bec2d45e7529d8e3003cbd43c43ceabc8eb9087
Kumar Gautam Pathak <36535562+gautam4941@users.noreply.github.com>
2020-03-16 00:54:25 +0530

    Update Prog4.py

--- Pooja_Assignment/Assignment 1/Prog4.py
+++ Pooja_Assignment/Assignment 1/Prog4.py
@@ -2,9 +2,13 @@ l1 = [1, 2, 3, 5, 9]
 l2 = [3, 6, 9, 12]
 l3 = []
 
-for i in range( len( l1 ) ):
+for i in range(len(l1)):
 
-    if( ( l1[i] in l2 ) and ( l1[i] not in l3 ) ):
-        l3.append( l1[i] )
+    if (l1[i] in l2):
+        if ((l2.count(l1[i])) == (l1.count(l1[i]))):
+            l3.append(l1[i])
 
-print( l3 )
+        elif( l1[i] not in l3 ):
+            l3.append(l1[i])
+
+print(l3)
